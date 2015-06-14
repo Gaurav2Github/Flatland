0092451450b33d31bcd1db9e99f112015f81c9e8
[Gaurav Tandon] <[gauravt.4u@gmail.com]>
2015-06-15 11:52:10 +1200

    refactoring panel product panels.

--- js/app.js
+++ js/app.js
@@ -212,15 +212,6 @@
 
 	});
 
-	app.controller('PanelController', function(){
-		this.tab = 1;
-		this.selectTab = function(setTab){
-			this.tab = setTab;
-		};
-		this.isSelected = function(checkTab){
-			return this.tab === checkTab;
-		};
-	});
 
 	app.controller('ReviewController', function(){
 		this.review= {};
