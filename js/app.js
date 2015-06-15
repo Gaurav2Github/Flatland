d0f12ac7aab2084af0160189540c101d4dd1da6b
[Gaurav Tandon] <[gauravt.4u@gmail.com]>
2015-06-15 12:18:38 +1200

    Refactoring Product Gallery code

--- js/app.js
+++ js/app.js
@@ -261,6 +261,26 @@ app.directive('productPanels', function(){
 	};
 });
 
+app.directive('productGallery', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/product-gallery.html',
+      controller: function(){
+          this.current = 0;
+          this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
+app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+  });
 
 })();
 
