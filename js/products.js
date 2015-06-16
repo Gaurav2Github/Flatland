8d86fc537fe5f7bc2fa55659ca4568851a86ffd4
[Gaurav Tandon] <[gauravt.4u@gmail.com]>
2015-06-16 17:23:35 +1200

    Modularizing angular code

---
+++ js/products.js
@@ -0,0 +1,58 @@
+(function(){
+  var app = angular.module('gemStore-products',[]);
+
+  app.directive('productDescription', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'templates/product-description.html'
+    };
+  });
+
+  app.directive('productReviews', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'templates/product-reviews.html'
+    };
+  });
+
+  app.directive('productSpecs', function(){
+    return{
+      restrict: 'A',
+      templateUrl: 'templates/product-specs.html'
+    };
+  });
+
+app.directive('productPanels', function(){
+  return{
+    restrict: 'E',
+    templateUrl: 'templates/product-panels.html',
+    controller: function(){
+      this.tab = 1;
+      this.selectTab = function(setTab){
+        this.tab = setTab;
+      };
+      this.isSelected = function(checkTab){
+        return this.tab === checkTab;
+      };
+    }, 
+    controllerAs: 'panel'
+  };
+});
+
+app.directive('productGallery', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/product-gallery.html',
+      controller: function(){
+          this.current = 0;
+          this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
+
+})();
+	
